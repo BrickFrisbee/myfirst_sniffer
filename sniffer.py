ce8ce82dde7ab92410867add11ae57bb7d186f68
BrickFrisbee <erhillma@asu.edu>
2023-08-10 15:06:26 -0700

    Ethernet Frame: Destination, Srouce, Protocol running...

--- sniffer.py
+++ sniffer.py
@@ -2,6 +2,15 @@ import socket
 import struct
 import textwrap
 
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65546)
+        dest_mac, sndr_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame:')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, sndr_mac, eth_proto))
+
 #Unpacking ethernet frame:
 def ethernet_frame(data):   #grabs first 14 bytes and unpacks it...
     dest_mac, sndr_mac, proto = struct.unpack('! 6s 6s H', data[:14])  #6s = 2 bytes, H = 2 bytes, small unsigned int
@@ -12,3 +21,4 @@ def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
 
+main()
